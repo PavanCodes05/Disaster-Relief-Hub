df09aec0458a115ba603babaee93394043bc596d
PavanCodes05 <pavancodes05@gmail.com>
2024-12-08 09:15:36 +0530

    Messages Backend Set

--- Backend/Controllers/messageController.js
+++ Backend/Controllers/messageController.js
@@ -1,5 +1,5 @@
 import User from "../Models/userModel.js";
-
+import Message from "../Models/messageModel.js";
 const getChats = async (req, res) => {
   try {
     const userId = req.user._id;
@@ -16,4 +16,41 @@ const getChats = async (req, res) => {
   }
 };
 
-export { getChats };
+const sendMessage = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { message } = req.body;
+
+    const newMessage = new Message({
+      from: req.user._id,
+      to: id,
+      message,
+    });
+
+    await newMessage.save();
+
+    res.status(200).json({ newMessage });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const getMessages = async (req, res) => {
+  try {
+    const sender = req.user._id;
+    const { id: receiver } = req.params;
+
+    const messages = await Message.find({
+      $or: [
+        { from: sender, to: receiver },
+        { from: receiver, to: sender },
+      ],
+    });
+
+    return res.status(200).json({ messages });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export { getChats, sendMessage, getMessages };
--- Backend/Routes/messageRoute.js
+++ Backend/Routes/messageRoute.js
@@ -1,9 +1,15 @@
 import express from "express";
 
 import authenticate from "../Middlewares/authenticationMiddleware.js";
-import { getChats } from "../Controllers/messageController.js";
+import {
+  getChats,
+  sendMessage,
+  getMessages,
+} from "../Controllers/messageController.js";
 const router = express.Router();
 
 router.get("/all", authenticate, getChats);
+router.post("/send/:id", authenticate, sendMessage);
+router.get("/getmessages/:id", authenticate, getMessages);
 
 export default router;
